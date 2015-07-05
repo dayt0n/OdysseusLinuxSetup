e968f5e4fa605f55d819c01ba27ca222b04846bc
Dayt0n <daytonhasty@gmail.com>
2015-07-04 22:38:49 -0500

    Cleaner

--- OdysseusSetup.cpp
+++ OdysseusSetup.cpp
@@ -32,10 +32,7 @@ int main()
     switch (userInput) {
         case 1:
             system("sudo apt-get update");
-            system("sudo apt-get -y install libimobiledevice-dev");
-            system("sudo apt-get -y install libssl-dev");
-            system("sudo apt-get -y install libusb-1.0.0");
-            system("sudo apt-get -y install libzip2");
+            system("sudo apt-get -y install libimobiledevice-dev libssl-dev libusb-1.0.0 libzip2");
             system("sudo apt-get -y install usbmuxd automake autoconf libtool pkg-config openssl build-essential libplist++-dev libzip-dev libcurl4-openssl-dev");
             system("clear");
             link("/lib/x86_64-linux-gnu/libssl.so.1.0.0", "/lib/x86_64-linux-gnu/libssl.so.1");
